fc31fff24cab927f57319ab8cebb3dfdb4149179
NABAO <xyinaxy@gmail.com>
2021-12-01 19:02:57 -0800

    revert to old controller code

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -1,10 +1,6 @@
 const models = require('../models');
 
 const get = (req, res) => {
-  if (req.query.product_id == null) {
-    res.status(400).send('Invalid product id');
-  }
-
   const productId = req.query.product_id;
   if (typeof parseInt(productId) === 'number') {
     let params = req.query;
@@ -12,17 +8,8 @@ const get = (req, res) => {
       if (err) {
         res.status(500).send(`Error getting reviews for product ${productId}: `, err);
 
-      } else if (result && result.results) {
+      } else {
         // console.log('what is params', params);
-        let nReviews = result.results.length;
-        for (var i = 0; i < nReviews; i++) {
-          if (!result.results[i].photos) {
-            result.results[i].photos = [];
-          }
-          if (result.results[i].response === 'null') {
-            result.results[i].response = null;
-          }
-        }
         res.status(200).send(result);
       }
     })
